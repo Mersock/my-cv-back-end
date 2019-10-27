6f9697f60cd3f127cea17a9eb5a3d0190f52ba1e
Phumthawan <knzphumthawan@gmail.com>
2019-10-27 21:39:06 +0700

    add babel

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
-const express = require('express')
+import express from 'express'
+import bodyParser from 'body-parser'
+
 const app = express()
 const port = process.env.PORT || 3400
+
+app.use(bodyParser.json())
+
 app.set('port', port)
 
 app.get('/', (req, res) => {
-    res.send('Hello World.')
+    res.send('Hello World .')
 })
 
 app.listen(app.get('port'),() => {
