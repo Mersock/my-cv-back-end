605a8fdfbf8a23f2ace377329045fe1f066143ac
Phumthawan <knzphumthawan@gmail.com>
2019-11-06 01:02:15 +0700

    Update app.js
    handle error.

--- src/app.js
+++ src/app.js
@@ -7,8 +7,26 @@ const app = express()
 
 const postsRouter = require('./routes/v1/posts')
 
+app.use(bodyParser.urlencoded({
+    extended: true
+  }))
 app.use(bodyParser.json())
 
+app.use((err, req, res, next) => {
+    if(err.statusCode == 400){
+        res.status(err.statusCode).send({
+            status_code: err.statusCode,
+            message:'Bad Request.'
+        })
+    }
+    if(err.statusCode == 500){
+        res.status(err.statusCode).send({
+            status_code: err.statusCode,
+            message:'Internal Server Error.'
+        })
+    }
+    next();
+})
 
 app.get('/', (req, res) => {
     res.send('This is My-CV APIs.')
@@ -16,12 +34,10 @@ app.get('/', (req, res) => {
 
 app.use(postsRouter)
 
-app.get('*', (req, res) => {
+app.get('*', function(req, res, next) {
     res.status(404).send({
-        'status_code': 404, 
-        'message': 'Not Found.'
+        status_code: 404,
+        message:'Page Not Found.'
     })
-})
-
-
+  });
 module.exports = app
