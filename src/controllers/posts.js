560862bd2df1f74f8c6163bcc394577e321c7829
Phumthawan <knzphumthawan@gmail.com>
2019-11-06 01:03:05 +0700

    Update posts.js
    add post controller

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,3 +1,5 @@
+const Post = require('../models/posts');
+
 
 exports.getAll  = async (req, res) => {
     const posts = await {
@@ -6,3 +8,18 @@ exports.getAll  = async (req, res) => {
     }
     res.json(posts)
 };
+
+exports.insert = async (req,res) => {
+    const post = new Post(req.body)
+    try {
+        await post.save();
+        res.status(201).json(post)
+    } catch (error) {
+        console.log(error)
+        const {errors} = error
+        res.status(400).send({
+            status_code:400,
+            errors
+        })
+    }
+}
