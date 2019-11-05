ffb96e15d482cab867dee2b9bd3c81c2a75e4154
Phumthawan <knzphumthawan@gmail.com>
2019-11-06 01:03:52 +0700

    Update posts.js
    add rout create posts.

--- src/routes/v1/posts.js
+++ src/routes/v1/posts.js
@@ -3,5 +3,7 @@ const router = new express.Router();
 const postsController = require('../../controllers/posts');
 
 router.get('/v1/posts', postsController.getAll)
+router.post('/v1/posts', postsController.insert)
+
 
 module.exports = router
