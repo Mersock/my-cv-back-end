2c158ebbe1a7601848457dd407986bcfddc53328
Phumthawan <knzphumthawan@gmail.com>
2019-11-27 13:24:06 +0700

    CRUD Users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,15 +1,61 @@
 const User = require('../models/users')
+const ObjectId = require('mongoose').Types.ObjectId
 
 exports.list = async (req, res) => {
-    res.send('xxx')
+    try {
+        const user = await User.find()
+        res.status(200).json({ data: user })
+    } catch (error) {
+        const errors = []
+        errors.push(error)
+        res.status(400).send({
+            statusCode: 400,
+            errors
+        })
+    }
 }
 
 exports.show = async (req, res) => {
-    res.send(req.params.id)
+    const _id = req.params.id
+
+    if (!ObjectId.isValid(_id)) {
+        return res.status(404).send({
+            statusCode: 404,
+            message: 'Not Found.'
+        })
+    }
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send({
+                statusCode: 404,
+                message: 'Not Found.'
+            })
+        }
+        res.status(200).send({ data: user })
+    } catch (error) {
+        const errors = []
+        errors.push(error)
+        res.status(400).send({
+            statusCode: 400,
+            errors
+        })
+    }
 }
 
 exports.create = async (req, res) => {
-    res.send(req.body)
+    const user = new User(req.body)
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (error) {
+        const errors = []
+        errors.push(error)
+        res.status(400).send({
+            statusCode: 400,
+            errors
+        })
+    }
 }
 
 exports.update = async (req, res) => {
@@ -17,5 +63,30 @@ exports.update = async (req, res) => {
 }
 
 exports.delete = async (req, res) => {
-    res.send(req.params.id)
+    const _id = req.params.id
+
+    if (!ObjectId.isValid(_id)) {
+        return res.status(404).send({
+            statusCode: 404,
+            message: 'Not Found.'
+        })
+    }
+
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        if (!user) {
+            return res.status(404).send({
+                statusCode: 404,
+                message: 'Not Found.'
+            })
+        }
+        res.status(204).send()
+    } catch (error) {
+        const errors = []
+        errors.push(error)
+        res.status(400).send({
+            statusCode: 400,
+            errors
+        })
+    }
 }
