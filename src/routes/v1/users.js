35ea9fb8974f47e2d3e5f5003bee115462a7922e
Phumthawan <knzphumthawan@gmail.com>
2019-11-27 18:50:40 +0700

    add user create middleware

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = new express.Router()
 const userController = require('../../controllers/users')
+const { validateCreate } = require('../../validations/users')
 
 router.get('/v1/users', userController.list)
 router.get('/v1/users/:id', userController.show)
-router.post('/v1/users', userController.create)
+router.post('/v1/users', validateCreate, userController.create)
 router.patch('/v1/users/:id', userController.update)
 router.delete('/v1/users/:id', userController.delete)
 
