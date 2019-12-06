c2bd66a26163d66f664c65bdb4835584b910cfd0
Phumthawan <knzphumthawan@gmail.com>
2019-12-07 00:20:27 +0700

    add hook mongodb middleware

--- src/models/users.js
+++ src/models/users.js
@@ -1,33 +1,58 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs')
 
-const userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true,
-        trim:true
+
+const userSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true,
+        trim: true
     },
-    lastname:{
-        type:String,
-        required:true,
-        trim:true
+    lastname: {
+        type: String,
+        required: true,
+        trim: true
     },
-    username:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:true
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:6,
-        trim:true
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true
     }
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
 
-const User = mongoose.model('User',userSchema,'users')
+userSchema.options.toJSON = {
+    transform: function (doc, user, options) {
+        user.id = user._id
+        delete user._id
+        delete user.password
+        delete user.__v
+        return user
+    }
+}
+
+userSchema.pre('save', async function (next) {
+    const user = this
+    user.password = await bcrypt.hash(user.password, 10)
+    next()
+})
+
+userSchema.pre('findOneAndUpdate', async function (next) {
+    const user = this.getUpdate()
+    if (user.$set.password) {
+        user.$set.password = await bcrypt.hash(user.$set.password, 10)
+    }
+})
+
+const User = mongoose.model('User', userSchema, 'users')
 
 module.exports = User;
