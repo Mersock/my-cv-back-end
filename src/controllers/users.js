984853643eb966b4f58ed7d3471bc4546a51db73
Phumthawan <knzphumthawan@gmail.com>
2019-12-15 10:46:29 +0700

    remove bcrypt

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcryptjs')
 const User = require('../models/users')
 const { responseWithError, responseWithCustomError, responseCollection } = require('../utils/response')
 
