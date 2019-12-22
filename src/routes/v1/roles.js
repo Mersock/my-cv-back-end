26e234c69443b303ffd17f8f528f03e965624c3a
Phumthawan <knzphumthawan@gmail.com>
2019-12-22 17:26:21 +0700

    add roles route.

---
+++ src/routes/v1/roles.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = new express.Router()
+const roleController = require('../../controllers/roles')
+const { authLogin } = require('../../middlewares/authentications')
+const { validateCreate, validateShow, validateUpdate, validateDelete } = require('../../validations/roles')
+
+router.get('/v1/roles', authLogin, roleController.list)
+router.get('/v1/roles/:id', authLogin, validateShow, roleController.show)
+router.patch('/v1/roles/:id', authLogin, validateUpdate, roleController.update)
+router.post('/v1/roles', authLogin, validateCreate, roleController.create)
+router.delete('/v1/roles/:id', authLogin, validateDelete, roleController.delete)
+
+module.exports = router
