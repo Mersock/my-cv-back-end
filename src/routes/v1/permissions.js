a1bbbf4f0da8a3e2ecdbef345078db30cc8d7297
Phumthawan <knzphumthawan@gmail.com>
2020-01-01 16:13:11 +0700

    addd permissions middleware

--- src/routes/v1/permissions.js
+++ src/routes/v1/permissions.js
@@ -1,13 +1,37 @@
 const express = require('express')
+const guard = require('express-jwt-permissions')()
 const router = new express.Router()
 const permissionController = require('../../controllers/permissions')
 const { authLogin } = require('../../middlewares/authentications')
 const { validateCreate, validateShow, validateUpdate, validateDelete } = require('../../validations/permissions')
 
-router.get('/v1/permissions', authLogin, permissionController.list)
-router.get('/v1/permissions/:id', authLogin, validateShow, permissionController.show)
-router.patch('/v1/permissions/:id', authLogin, validateUpdate, permissionController.update)
-router.post('/v1/permissions', authLogin, validateCreate, permissionController.create)
-router.delete('/v1/permissions/:id', authLogin, validateDelete, permissionController.delete)
+const listMiddleware = [
+    authLogin,
+    guard.check(['admin'], ['permissions.list'])
+]
+
+const readMiddleware = [
+    authLogin,
+    guard.check(['admin'], ['permissions:read'])
+]
+
+const createMiddleware = [
+    authLogin,
+    guard.check(['admin'], ['permissions.create'])
+]
+const updateMiddleware = [
+    authLogin,
+    guard.check(['admin'], ['permissions:update'])
+]
+const deleteMiddleware = [
+    authLogin,
+    guard.check(['admin'], ['permissions:delete'])
+]
+
+router.get('/v1/permissions', listMiddleware, permissionController.list)
+router.get('/v1/permissions/:id', readMiddleware, validateShow, permissionController.show)
+router.patch('/v1/permissions/:id', updateMiddleware, validateUpdate, permissionController.update)
+router.post('/v1/permissions', createMiddleware, validateCreate, permissionController.create)
+router.delete('/v1/permissions/:id', deleteMiddleware, validateDelete, permissionController.delete)
 
 module.exports = router
