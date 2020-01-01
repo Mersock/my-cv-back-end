81e959f1556d75b8a55cab43f8bab77c8d1e6a46
Phumthawan <knzphumthawan@gmail.com>
2020-01-01 23:02:05 +0700

    change authentication es6

--- src/utils/authentications.js
+++ src/utils/authentications.js
@@ -1,45 +1,45 @@
-const jwt = require('jsonwebtoken')
-const _ = require('lodash')
-const fs = require('fs')
-const path = require('path')
-const randtoken = require('rand-token')
-const client = require('../db/redis')
+import jwt from 'jsonwebtoken'
+import client from '../db/redis'
+import _ from 'lodash'
+import fs from 'fs'
+import path from 'path'
+import randtoken from 'rand-token'
+
 const privateKey = fs.readFileSync(path.join(__dirname, '../keys') + '/private.key', 'utf8');
 
-
-exports.signOption = (user) => {
-    const permisisons = this.responstPermisisons(user.permissions)
-    const payload = {
+export const signOption = (user) => {
+    let permisisons = responsePermisisons(user.permissions)
+    let payload = {
         id: user._id,
         username: user.username,
         permissions: permisisons
     }
-    const exp = Math.floor(Date.now() / 1000) + (60 * 60 * 4)
-    const signOptions = {
+    let exp = Math.floor(Date.now() / 1000) + (60 * 60 * 4)
+    let signOptions = {
         expiresIn: exp,
         algorithm: "RS256"
     };
     return jwt.sign(payload, privateKey, signOptions)
 }
 
-exports.saveRefreshToken = (user) => {
-    const refreshToken = randtoken.uid(256)
-    const exp = (60 * 60 * 24)
+export const saveRefreshToken = user => {
+    let refreshToken = randtoken.uid(256)
+    let exp = (60 * 60 * 24)
     client.set(`refreshToken_${user._id}_${refreshToken}`, JSON.stringify(user), 'EX', exp)
     return refreshToken
 }
 
-exports.getUserFromRefreshToken = (userId, refreshToken) => {
-    const user = client.getAsync(`refreshToken_${userId}_${refreshToken}`).then(function (res) {
+export const getUserFromRefreshToken = (userId, refreshToken) => {
+    let user = client.getAsync(`refreshToken_${userId}_${refreshToken}`).then(function (res) {
         return res
     })
     return user
 }
 
-exports.destroyToken = (userId, refreshToken) => {
+export const destroyToken = (userId, refreshToken) => {
     client.del(`refreshToken_${userId}_${refreshToken}`);
 }
 
-exports.responstPermisisons = permisisons => {
+export const responsePermisisons = permisisons => {
     return _.map(permisisons, 'name')
 }
