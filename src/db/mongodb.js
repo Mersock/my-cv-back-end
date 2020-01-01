e2728ea25c7ded6fd76457da91312859b47aec87
Phumthawan <knzphumthawan@gmail.com>
2020-01-01 22:09:59 +0700

    chagne connect with mongdb

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
-const connectionURL = process.env.MONGO_URL
+import mongoose from 'mongoose';
 
-const option = {
-    user: process.env.MONGO_DB_USER,
-    pass: process.env.MONGO_DB_PASSWORD,
-    useNewUrlParser: true
+class Mongodb {
+    constructor() {
+        const url = process.env.MONGO_URL
+        const option = {
+            user: process.env.MONGO_DB_USER,
+            pass: process.env.MONGO_DB_PASSWORD,
+            useNewUrlParser: true
+        }
+        mongoose.connect(url, option)
+    }
 }
 
-mongoose.connect(connectionURL, option);
+export default new Mongodb();
