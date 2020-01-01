a34c4fa5f996ccf1e8ff59e82994ed9fa945f859
Phumthawan <knzphumthawan@gmail.com>
2020-01-01 23:13:18 +0700

    improve with authentications

--- src/controllers/authentications.js
+++ src/controllers/authentications.js
@@ -1,19 +1,19 @@
-const bcrypt = require('bcryptjs')
-const _ = require('lodash')
-const User = require('../models/users')
-const { responseWithCustomError, responseValidateError } = require('../utils/response')
-const client = require('../utils/authentications')
+import bcrypt from 'bcryptjs'
+import _ from 'lodash'
+import { responseWithCustomError } from '../utils/response'
+import { signOption, saveRefreshToken, getUserFromRefreshToken } from '../utils/authentications'
+import User from '../models/users'
 
-exports.login = async (req, res) => {
+export const login = async (req, res) => {
     try {
-        const { username, password } = req.body
+        let { username, password } = req.body
         return User.findOne({ username }).populate({
             path: 'permissions',
         }).exec(function (err, user) {
-            const isMatch = bcrypt.compareSync(password, user.password)
+            let isMatch = bcrypt.compareSync(password, user.password)
             if (isMatch) {
-                const accessToken = client.signOption(user)
-                const refreshToken = client.saveRefreshToken(user)
+                let accessToken = signOption(user)
+                let refreshToken = saveRefreshToken(user)
                 return res.status(200).send({
                     accessToken,
                     refreshToken
@@ -27,12 +27,12 @@ exports.login = async (req, res) => {
     }
 }
 
-exports.refreshToken = async (req, res) => {
+export const refreshToken = async (req, res) => {
     try {
-        const { refreshToken, userId } = req.body
-        const userOject = await client.getUserFromRefreshToken(userId, refreshToken)
-        const user = JSON.parse(userOject)
-        const accessToken = client.signOption(user)
+        let { refreshToken, userId } = req.body
+        let userOject = await getUserFromRefreshToken(userId, refreshToken)
+        let user = JSON.parse(userOject)
+        let accessToken = client.signOption(user)
         res.status(200).send({
             accessToken
         })
@@ -42,11 +42,11 @@ exports.refreshToken = async (req, res) => {
     }
 }
 
-exports.logout = async (req, res) => {
+export const logout = async (req, res) => {
     try {
-        const { userId, refreshToken } = req.body
-        const userOject = await client.getUserFromRefreshToken(userId, refreshToken)
-        const user = JSON.parse(userOject)
+        let { userId, refreshToken } = req.body
+        let userOject = await getUserFromRefreshToken(userId, refreshToken)
+        let user = JSON.parse(userOject)
         if (user.id) {
             client.destroyToken(user.id, refreshToken)
             return res.status(204).send()
