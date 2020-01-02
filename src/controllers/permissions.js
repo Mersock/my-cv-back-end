b7e82a4d69843ae419c4c9ca49bc811731f8ce89
Phumthawan <knzphumthawan@gmail.com>
2020-01-02 08:51:59 +0700

    improve permissions

--- src/controllers/permissions.js
+++ src/controllers/permissions.js
@@ -3,34 +3,34 @@ import { responseCollection, responseWithError, responseWithCustomError } from '
 
 export const list = async (req, res) => {
     try {
-        const permissions = await Permission.find()
+        let permissions = await Permission.find()
         res.status(200).json(responseCollection(permissions))
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const show = async (req, res) => {
-    const _id = req.params.id
+    let _id = req.params.id
     try {
-        const permissions = await Permission.findById(_id)
+        let permissions = await Permission.findById(_id)
         if (!permissions) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(200).send(responseCollection(permissions))
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const update = async (req, res) => {
-    const _id = req.params.id
+    let _id = req.params.id
     try {
         const permissions = await Permission.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, useFindAndModify: false })
         if (!permissions) {
@@ -39,7 +39,7 @@ export const update = async (req, res) => {
         res.status(200).send(responseCollection(permissions))
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
@@ -47,7 +47,7 @@ export const update = async (req, res) => {
 }
 
 export const create = async (req, res) => {
-    const permissions = new Permission(req.body)
+    let permissions = new Permission(req.body)
     try {
         await permissions.save()
         res.status(201).send(responseCollection(permissions))
@@ -57,16 +57,16 @@ export const create = async (req, res) => {
 }
 
 export const destroy = async (req, res) => {
-    const _id = req.params.id
+    let _id = req.params.id
     try {
-        const permissions = await Permission.findByIdAndDelete(_id)
+        let permissions = await Permission.findByIdAndDelete(_id)
         if (!permissions) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(204).send()
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
--- src/validations/permissions.js
+++ src/validations/permissions.js
@@ -6,7 +6,7 @@ import Permission from '../models/permissions'
 export const validateCreate = validatetions([
     body('name').notEmpty().withMessage('name is required.')
         .custom(async name => {
-            const permissions = await Permission.findOne({ name })
+            let permissions = await Permission.findOne({ name })
             if (!_.isEmpty(permissions)) {
                 throw new Error(`name is ${name} has been taken`)
             }
@@ -17,8 +17,8 @@ export const validateUpdate = validatetions([
     param('id').isMongoId().withMessage('ID is invalid value.'),
     body('name').optional().notEmpty().withMessage('name must be required.')
         .custom(async (value, { req }) => {
-            const id = req.params.id
-            const roles = await Roles.find({ _id: { $ne: id }, name: { $in: [value] } })
+            let id = req.params.id
+            let roles = await Roles.find({ _id: { $ne: id }, name: { $in: [value] } })
             if (roles.length > 0) {
                 throw new Error(`name is ${value} has been taken`)
             }
