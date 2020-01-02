051d3860b3c4c6e31f1d2a2faa13eda2f2daa0ac
Phumthawan <knzphumthawan@gmail.com>
2020-01-02 08:51:42 +0700

    improve users es6

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,56 +3,56 @@ import { responseWithError, responseWithCustomError, responseCollection } from '
 
 export const list = async (req, res) => {
     try {
-        const user = await User.find()
+        let user = await User.find()
         res.status(200).json(responseCollection(user))
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const show = async (req, res) => {
-    const _id = req.params.id
+    let _id = req.params.id
     try {
-        const user = await User.findById(_id)
+        let user = await User.findById(_id)
         if (!user) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(200).send(responseCollection(user))
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const create = async (req, res) => {
-    const user = new User(req.body)
+    let user = new User(req.body)
     try {
         await user.save()
         res.status(201).send(responseCollection(user))
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const update = async (req, res) => {
-    const _id = req.params.id
+    let _id = req.params.id
     try {
-        const user = await User.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, useFindAndModify: false })
+        let user = await User.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, useFindAndModify: false })
         if (!user) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(200).send(responseCollection(user))
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
@@ -60,16 +60,16 @@ export const update = async (req, res) => {
 }
 
 export const destroy = async (req, res) => {
-    const _id = req.params.id
+    let _id = req.params.id
     try {
-        const user = await User.findByIdAndDelete(_id)
+        let user = await User.findByIdAndDelete(_id)
         if (!user) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(204).send()
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
@@ -87,14 +87,14 @@ export const listWithPermission = async (req, res) => {
             })
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const showWithPermissions = async (req, res) => {
-    const _id = req.params.id
+    let _id = req.params.id
     try {
         return await User.findOne({ _id })
             .populate({
@@ -109,7 +109,7 @@ export const showWithPermissions = async (req, res) => {
             })
     } catch (error) {
         console.log(error)
-        const errors = []
+        let errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
--- src/validations/users.js
+++ src/validations/users.js
@@ -8,7 +8,7 @@ import mongoose from 'mongoose'
 export const validateCreate = validatetions([
     body('username').isLength({ min: 4 }).withMessage('username must be least 4 chars.')
         .custom(async username => {
-            const user = await User.findOne({ username })
+            let user = await User.findOne({ username })
             if (!_.isEmpty(user)) {
                 throw new Error(`username is ${username} has been taken`)
             }
@@ -21,16 +21,16 @@ export const validateCreate = validatetions([
         .isArray().withMessage('Permissions must be array.')
 
         .custom(async (value, { req }) => {
-            const ids = _.isArray(value) ? value : [];
-            const findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+            let ids = _.isArray(value) ? value : [];
+            let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
             if (findDuplicates(ids).length != 0) {
                 throw new Error(`permissions ${findDuplicates(value).toString()} duplicate.`)
             }
         })
         .custom(async (value, { req }) => {
-            const ids = _.isArray(value) ? value : [];
-            const id = ids.map(_id => new mongoose.Types.ObjectId(_id))
-            const permissions = await Permissions.find({ "_id": { $in: id } })
+            let ids = _.isArray(value) ? value : [];
+            let id = ids.map(_id => new mongoose.Types.ObjectId(_id))
+            let permissions = await Permissions.find({ "_id": { $in: id } })
             permissionsId = _.map(permissions, '_id')
             if (!_.isEqual(_.sortBy(id), _.sortBy(permissionsId))) {
                 throw new Error(`permissions invalid value`)
@@ -42,8 +42,8 @@ export const validateUpdate = validatetions([
     param('id').isMongoId().withMessage('ID is invalid value.'),
     body('username').optional().isLength({ min: 4 }).withMessage('username must be least 4 chars.')
         .custom(async (value, { req }) => {
-            const id = req.params.id
-            const user = await User.find({ _id: { $ne: id }, username: { $in: [value] } })
+            let id = req.params.id
+            let user = await User.find({ _id: { $ne: id }, username: { $in: [value] } })
             if (user.length > 0) {
                 throw new Error(`username is ${value} has been taken`)
             }
@@ -54,16 +54,16 @@ export const validateUpdate = validatetions([
     body('permissions').optional()
         .isArray().withMessage('Permissions must be array.')
         .custom(async (value, { req }) => {
-            const ids = _.isArray(value) ? value : [];
-            const findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+            let ids = _.isArray(value) ? value : [];
+            let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
             if (findDuplicates(ids).length != 0) {
                 throw new Error(`permissions ${findDuplicates(value).toString()} duplicate.`)
             }
         })
         .custom(async (value, { req }) => {
-            const ids = _.isArray(value) ? value : [];
-            const id = ids.map(_id => new mongoose.Types.ObjectId(_id))
-            const permissions = await Permissions.find({ "_id": { $in: id } })
+            let ids = _.isArray(value) ? value : [];
+            let id = ids.map(_id => new mongoose.Types.ObjectId(_id))
+            let permissions = await Permissions.find({ "_id": { $in: id } })
             permissionsId = _.map(permissions, '_id')
             if (!_.isEqual(_.sortBy(id), _.sortBy(permissionsId))) {
                 throw new Error(`permissions invalid value`)
