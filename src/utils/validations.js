b570058331c38762c31e23fa80c53000d5045d69
Phumthawan <knzphumthawan@gmail.com>
2020-01-02 08:52:39 +0700

    improve validations

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -3,11 +3,11 @@ import { validationResult } from 'express-validator'
 export const validatetions = validations => async (req, res, next) => {
     await Promise.all(validations.map(validation => validation.run(req)));
 
-    const errors = validationResult(req);
+    let errors = validationResult(req);
     if (errors.isEmpty()) {
         return next();
     }
-    const extractedErrors = []
+    let extractedErrors = []
     errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
     return res.status(422).json({ errors: extractedErrors })
 };
