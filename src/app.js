0f18fffbc71b5ce0f7f1abdd7490d129a2aa459e
knzphumthawan@gmail.com <paradon-.ba@hotmail.com>
2020-01-15 00:18:04 +0700

    allow cors

--- src/app.js
+++ src/app.js
@@ -21,6 +21,28 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json())
 
+app.all('*', function (req, res, next) {
+    let responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE",
+        "AccessControlAllowCredentials": true
+    };
+
+    res.header("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin", responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+});
+
+
 app.use(handleRequest)
 
 app.get('/', (req, res) => {
