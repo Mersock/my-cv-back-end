7ee0b6507d5feb7a4e0e976deeb2a6a2f4fa4f99
knzphumthawan <knzphumthawan@gmail.com>
2020-01-25 18:10:55 +0700

    permissions

--- src/controllers/v1/permissions.js
+++ src/controllers/v1/permissions.js
@@ -5,39 +5,39 @@ import { responseCollection, responseWithError, responseWithCustomError } from '
 
 export const list = async (req, res) => {
     try {
-        let { page, limit } = req.query
-        let { name } = req.query
-        let { sortBy, sortType } = req.query
-        let filterLike = queryLike({ name })
-        let sort = querySort(sortBy, sortType)
-        let permissions = await Permission.paginate(_.merge(filterLike), setOptions(page, limit, sort))
+        const { page, limit } = req.query
+        const { name } = req.query
+        const { sortBy, sortType } = req.query
+        const filterLike = queryLike({ name })
+        const sort = querySort(sortBy, sortType)
+        const permissions = await Permission.paginate(_.merge(filterLike), setOptions(page, limit, sort))
         res.status(200).json(permissions)
     } catch (error) {
         console.log(error)
-        let errors = []
+        const errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const show = async (req, res) => {
-    let _id = req.params.id
+    const _id = req.params.id
     try {
-        let permissions = await Permission.findById(_id)
+        const permissions = await Permission.findById(_id)
         if (!permissions) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(200).send(responseCollection(permissions))
     } catch (error) {
         console.log(error)
-        let errors = []
+        const errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const update = async (req, res) => {
-    let _id = req.params.id
+    const _id = req.params.id
     try {
         const permissions = await Permission.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, useFindAndModify: false })
         if (!permissions) {
@@ -46,7 +46,7 @@ export const update = async (req, res) => {
         res.status(200).send(responseCollection(permissions))
     } catch (error) {
         console.log(error)
-        let errors = []
+        const errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
@@ -54,7 +54,7 @@ export const update = async (req, res) => {
 }
 
 export const create = async (req, res) => {
-    let permissions = new Permission(req.body)
+    const permissions = new Permission(req.body)
     try {
         await permissions.save()
         res.status(201).send(responseCollection(permissions))
@@ -64,16 +64,16 @@ export const create = async (req, res) => {
 }
 
 export const destroy = async (req, res) => {
-    let _id = req.params.id
+    const _id = req.params.id
     try {
-        let permissions = await Permission.findByIdAndDelete(_id)
+        const permissions = await Permission.findByIdAndDeconste(_id)
         if (!permissions) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(204).send()
     } catch (error) {
         console.log(error)
-        let errors = []
+        const errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
