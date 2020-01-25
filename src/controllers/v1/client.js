8720ca6ec19d3272121616ef5b2b008926533cb6
knzphumthawan <knzphumthawan@gmail.com>
2020-01-25 18:07:53 +0700

    client

--- src/controllers/v1/client.js
+++ src/controllers/v1/client.js
@@ -8,49 +8,49 @@ import { responseWithError, responseWithCustomError, responseCollection } from '
 
 export const list = async (req, res) => {
     try {
-        let { page, limit } = req.query
-        let { sortBy, sortType } = req.query
-        let { name, active } = req.query
-        let filter = queryEquals({ active })
-        let filterLike = queryLike({ name })
-        let sort = querySort(sortBy, sortType)
-        let client = await Client.paginate(_.merge(filterLike, filter), setOptions(page, limit, sort))
+        const { page, limit } = req.query
+        const { sortBy, sortType } = req.query
+        const { name, active } = req.query
+        const filter = queryEquals({ active })
+        const filterLike = queryLike({ name })
+        const sort = querySort(sortBy, sortType)
+        const client = await Client.paginate(_.merge(filterLike, filter), setOptions(page, limit, sort))
         res.status(200).json(client)
     } catch (error) {
         console.log(error)
-        let errors = []
+        const errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const show = async (req, res) => {
-    let _id = req.params.id
+    const _id = req.params.id
     try {
-        let client = await Client.findById(_id)
+        const client = await Client.findById(_id)
         if (!client) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(200).send(responseCollection(client))
     } catch (error) {
         console.log(error)
-        let errors = []
+        const errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
 }
 
 export const update = async (req, res) => {
-    let _id = req.params.id
+    const _id = req.params.id
     try {
-        let client = await Client.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, useFindAndModify: false })
+        const client = await Client.findOneAndUpdate({ _id }, { $set: req.body }, { new: true, useFindAndModify: false })
         if (!client) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(200).send(responseCollection(client))
     } catch (error) {
         console.log(error)
-        let errors = []
+        const errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
@@ -58,7 +58,7 @@ export const update = async (req, res) => {
 
 export const create = async (req, res) => {
     req.body.secret = randtoken.uid(16)
-    let client = new Client(req.body)
+    const client = new Client(req.body)
     try {
         await client.save()
         res.status(201).send(responseCollection(client))
@@ -68,16 +68,16 @@ export const create = async (req, res) => {
 }
 
 export const destroy = async (req, res) => {
-    let _id = req.params.id
+    const _id = req.params.id
     try {
-        let client = await Client.findByIdAndDelete(_id)
+        const client = await Client.findByIdAndDeconste(_id)
         if (!client) {
             return res.status(404).send(responseWithCustomError('Not Found.', 404))
         }
         res.status(204).send()
     } catch (error) {
         console.log(error)
-        let errors = []
+        const errors = []
         errors.push(error)
         res.status(400).send(responseWithError(errors, 400))
     }
